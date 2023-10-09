409fd06a5b1605936a111a0e05e3b0e99b519cf5
Geet Sethi <geetsethi05@gmail.com>
2023-10-09 15:59:02 +0530

    feat: add support for multiple notification providers
    Now, any number of different notification providers can be registered
    and run simultaneously.

--- temp_track/agents/notifier.py
+++ temp_track/agents/notifier.py
@@ -1,4 +1,6 @@
+from typing import List
 import logging
+
 from uagents import Agent, Context
 
 from temp_track.messages import TrackerStatus, TrackerError
@@ -7,35 +9,41 @@ from temp_track.notification_providers import NotificationProvider
 notifier = Agent(name="notifier", seed="notifier-agent-seed")
 notifier._logger = logging.Logger("notifier-logger", logging.ERROR)
 
-notification_provider: NotificationProvider | None = None
+notification_providers: List[NotificationProvider] = []
 
 
-def set_notification_proivder(provider: NotificationProvider) -> None:
-    global notification_provider
-    notification_provider = provider
+def register_notification_proivder(provider: NotificationProvider) -> None:
+    global notification_providers
+    notification_providers.append(provider)
 
 
 @notifier.on_message(TrackerStatus)
 async def on_tracker_status_msg(
     _ctx: Context, _sender: str, msg: TrackerStatus
 ) -> None:
-    if notification_provider is None:
+    if len(notification_providers) == 0:
         raise Exception(
-            "`notification_proivder` must be set using `set_notification_provider` before starting notifier!"
+            "`notification_proivder` must be registered using `register_notification_provider` before starting notifier!"
         )
 
     # Do anything that needs to be done before sending the msg!
 
-    notification_provider.notify(msg)
+    [
+        notification_provider.notify(msg)
+        for notification_provider in notification_providers
+    ]
 
 
 @notifier.on_message(TrackerError)
 async def on_tracker_error_msg(_ctx: Context, _sender: str, msg: TrackerError) -> None:
-    if notification_provider is None:
+    if notification_providers is None:
         raise Exception(
-            "`notification_proivder` must be set using `set_notification_provider` before starting notifier!"
+            "`notification_proivder` must be registered using `register_notification_provider` before starting notifier!"
         )
 
     # Do anything that needs to be done before sending the msg!
 
-    notification_provider.notify_error(msg)
+    [
+        notification_provider.notify_error(msg)
+        for notification_provider in notification_providers
+    ]
